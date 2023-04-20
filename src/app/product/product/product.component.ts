493780a9f1befdecd4f538ac314faee7b93e5c23
terryliu <terryliu@fstop.com.tw>
2023-04-20 14:50:07 +0800

    add item()

--- src/app/product/product/product.component.ts
+++ src/app/product/product/product.component.ts
@@ -32,53 +32,53 @@ export class ProductComponent implements OnInit {
         productId: '01',
         productName: '商品A',
         productPrice: 123,
-        productCount: this.productCount,
+        productCount: 1,
         productDesc: '商品A細節',
       },
       {
-         productId: '02',
+        productId: '02',
         productName: '商品B',
         productPrice: 234,
-        productCount: this.productCount,
+        productCount: 1,
         productDesc: '商品B細節',
       },
       {
-         productId: '03',
+        productId: '03',
         productName: '商品C',
         productPrice: 124,
         productCount: 1,
         productDesc: '商品C細節',
       },
       {
-         productId: '04',
+        productId: '04',
         productName: '商品D',
         productPrice: 125,
         productCount: 1,
         productDesc: '商品D細節',
       },
       {
-         productId: '05',
+        productId: '05',
         productName: '商品E',
         productPrice: 128,
         productCount: 1,
         productDesc: '商品E細節',
       },
       {
-         productId: '06',
+        productId: '06',
         productName: '商品F',
         productPrice: 1111,
         productCount: 1,
         productDesc: '商品F細節',
       },
       {
-         productId: '07',
+        productId: '07',
         productName: '商品G',
         productPrice: 12312,
         productCount: 1,
         productDesc: '商品G細節',
       },
       {
-         productId: '08',
+        productId: '08',
         productName: '商品H',
         productPrice: 1233,
         productCount: 1,
@@ -87,28 +87,28 @@ export class ProductComponent implements OnInit {
     ] as product[]
   }
 
-  addcart(item:any): void {
+  addcart(item: any): void {
     const abcArray = {
       productName: item.productName,
-      productCount: this.productCount,
+      productCount: item.productCount,
       productPrice: item.productPrice,
-      productId:item.productId
+      productId: item.productId
     }
     console.log(abcArray)
     // console.log(item)
     // console.log(this.productCount)
   }
 
-  add(): void {
-    this.productCount++
+  add(item: any): void {
+    item.productCount++
   }
 
-  minus(): void {
-    if (this.productCount > 1) this.productCount--
+  minus(item: any): void {
+    if (item.productCount > 1) item.productCount--
   }
 
   getcoupon(): void {
-
+    window.alert('成功領取!')
   }
 
 
